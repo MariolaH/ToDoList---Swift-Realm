c3b1092fc9d551012edc5e521ac2b22eff9a3540
Mariola <mariolahullings@gmail.com>
2024-02-20 11:07:13 -0500

    Created cells in tableView

--- ToDoList - Swift/TodoListViewController.swift
+++ ToDoList - Swift/TodoListViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
+    
     let itemArray = ["Find Mike", "Buy Eggs", "Buy Banana"]
     
     override func viewDidLoad() {
@@ -33,6 +33,30 @@ class TodoListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
-
+    
+    //MARK - TableView Delegate Methods
+    
+    // fired whenever we click on any cell in the tableview
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //This just prints the number of the row that is selected
+        //        print("Selected Row:  \(indexPath.row)")
+        //Prints what is written in that row
+        //        print("Selected Row:  \(itemArray[indexPath.row])")
+        //if the tableview's current cell at the current selected indexPath already has an an accessory that is a checkmark
+        //check to see if the current cell that is selected has an accessoryType of checkmark
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            // if it does, change it to none to remove the checkmark
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            //otherwise if it doesn't have a checkmark, and we selected it, then its going to add a checkmark
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+    
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
 }
 
