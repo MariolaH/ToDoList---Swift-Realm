0b4e61c598f323122b7bafc88d64ad4c090aaed8
Mariola <mariolahullings@gmail.com>
2024-03-22 11:19:42 -0400

    Created search feature for todo list

--- ToDoList - Swift/Controllers/TodoListViewController.swift
+++ ToDoList - Swift/Controllers/TodoListViewController.swift
@@ -19,7 +19,7 @@ class TodoListViewController: UITableViewController {
         loadItems()
     }
     
-    //MARK - Tableview Datasource Methods
+    //MARK: - Tableview Datasource Methods
     
     //function for specifying the number of rows in a section
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -41,14 +41,17 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
-    //MARK - TableView Delegate Methods
+    //MARK: - TableView Delegate Methods
     // fired whenever we click on any cell in the tableview
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         //sets the done property on the current item in the itemArray to the opposite of what it is right now.
         //if it's true it becomes false. If it's false, it becomes true. All done by using the NOT(!) operator
-        
+        //This line of code changes the title to Completed when you click on the item
+//        itemArray[indexPath.row].setValue("Completed", forKey: "title")
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
         
         savedItems()
         
@@ -65,7 +68,7 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    //MARK - Add New Items
+    //MARK: - Add New Items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -95,7 +98,7 @@ class TodoListViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    //MARK - Model Manipulation Methods
+    //MARK: - Model Manipulation Methods
     
     //This function, savedItems(), is responsible for encoding an array of items (data model) and saving it to a file using Property List serialization.
     
@@ -110,13 +113,29 @@ class TodoListViewController: UITableViewController {
     }
     
      //this function is responsible for loading an array of Item objects from a file specified by dataFilePath, decoding the data, and storing it in the itemArray property
-    func loadItems() {
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
         do {
             itemArray = try context.fetch(request)
         } catch {
             print("Error fetching data from context \(error)")
         }
+        //reload our tableView so that we retrigger the cellForRowAt indexPath methods and we update our tableView with the current itemArray which contains the results from the search
+        tableView.reloadData()
     }
 }
 
+// MARK: - SearchBar
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        //created a predicate which specifies how we want to query our database
+        //this line of code adds the query to the request
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        //sort the data that comes back from the database any order we want
+        //add the sortDescriptor to the request
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        //run the request and fetch the results
+        loadItems(with: request)
+    }
+}
