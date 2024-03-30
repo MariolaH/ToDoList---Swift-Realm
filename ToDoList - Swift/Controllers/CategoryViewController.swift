dea86b691c03719d70a59aff0b26122ad449e2f0
Mariola <mariolahullings@gmail.com>
2024-03-30 16:22:41 -0400

    Added functionality to categories

--- ToDoList - Swift/Controllers/CategoryViewController.swift
+++ ToDoList - Swift/Controllers/CategoryViewController.swift
@@ -10,33 +10,60 @@ import CoreData
 
 class CategoryViewController: UITableViewController {
     
-    var categoryArray = ["Category", "Cat"]
+    var categories = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadCategory()
+        loadCategories()
     }
     
     //MARK: - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categoryArray.count
+        return categories.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        cell.textLabel?.text = categoryArray[indexPath.row]
+        cell.textLabel?.text = categories[indexPath.row].name
         return cell
     }
     
-    //MARK: - Data Manipulation Methods
+    //MARK: - TableView Delegate Methods
     
-    func saveCategory() {
-        
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
     }
     
-    func loadCategory() {
-        
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+    }
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+    func saveCategorie() {
+        do {
+            try context.save()
+        } catch {
+            print("Error print category \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadCategories() {
+            let request: NSFetchRequest<Category> = Category.fetchRequest()
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error loading categories \(error)")
+        }
+        tableView.reloadData()
     }
     
     //MARK: - Add New Categories
@@ -48,17 +75,22 @@ class CategoryViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
             
-        }
-        
-        alert.addTextField{ (alertTextField) in
-            alertTextField.placeholder = "Add Category"
-            textField = alertTextField
+            let newCategory = Category(context: self.context)
+            //whatever the user entered into that textfield inside the alert is going to be the name of the new category
+            newCategory.name = textField.text!
+            self.categories.append(newCategory)
+            self.saveCategorie()
         }
         
         alert.addAction(action)
+        
+        alert.addTextField{ (field) in
+            textField = field
+            field.placeholder = "Add a new Category"
+        }
+        
         present(alert, animated: true, completion: nil)
     }
     
-    //MARK: - TableView Delegate Methods
     
 }
