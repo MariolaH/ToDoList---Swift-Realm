3456cb9eb07e724318634d420691619430a54fe2
Mariola <mariolahullings@gmail.com>
2024-04-07 09:38:56 -0400

    Added Date

--- ToDoList - Swift/Data Model/Item.swift
+++ ToDoList - Swift/Data Model/Item.swift
@@ -12,6 +12,7 @@ class Item: Object {
     
     @objc dynamic var title: String = ""
     @objc dynamic var done: Bool = false
+    @objc dynamic var dateCreated = Date()
     //reverse category, each item has a parentCategory that is of type Category and comes from the property items
     var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
 }
